da522928d4144372812eb38257a2888c91a1bab5
zhou <zhou@ubuntu.com>
2021-02-17 23:47:57 +0100

    add download url

--- search-with-duden.user.js
+++ search-with-duden.user.js
@@ -8,6 +8,7 @@
 // @grant        GM_openInTab
 // @run-at       context-menu
 // @updateURL    https://github.com/Zhgong/tampermonkey-scripts/raw/master/search-with-duden.user.js
+// @downloadURL  https://github.com/Zhgong/tampermonkey-scripts/raw/master/search-with-duden.user.js
 // ==/UserScript==
 
 (function() {
