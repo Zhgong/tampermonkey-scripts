5c25d07f41a3540ec9c8a004d0f5ed40e52831c7
zhou <zhou@ubuntu.com>
2021-02-18 23:57:15 +0100

    update of script

--- youtube-skip-ads.user.js
+++ youtube-skip-ads.user.js
@@ -2,34 +2,22 @@
 // @name         YouTube - Ad Skip
 // @version      0.1
 // @description  Skips and removes ads on YouTube automatically
-// @author       Bobocato
+// @author       zhou
 // @match        https://www.youtube.com/*
 // @grant        none
-// @namespace https://greasyfork.org/users/167089
+// @namespace    http://tampermonkey.net/
 // @updateURL    https://github.com/Zhgong/tampermonkey-scripts/raw/master/youtube-skip-ads.user.js
 // @downloadURL  https://github.com/Zhgong/tampermonkey-scripts/raw/master/youtube-skip-ads.user.js
 // ==/UserScript==
 
-const equalText1 = "Skip Ads";
-const equalText2 = "Skip Ad";
-const equalText3 = "跳过广告";
+const labels = ["Skip Ads", "Skip Ad", "跳过广告"]
 
-function addNewStyle(newStyle) {
-    var styleElement = document.getElementById('styles_js');
-    if (!styleElement) {
-        styleElement = document.createElement('style');
-        styleElement.type = 'text/css';
-        styleElement.id = 'styles_js';
-        document.getElementsByTagName('head')[0].appendChild(styleElement);
-    }
-    styleElement.appendChild(document.createTextNode(newStyle));
-}
 
-function skipAd(){
+function skipAd() {
     console.log("Tried to skip a ad");
-    if(document.getElementsByClassName("ytp-ad-skip-button").length > 0){
+    if (document.getElementsByClassName("ytp-ad-skip-button").length > 0) {
         let text = document.getElementsByClassName("ytp-ad-skip-button")[0].childNodes[0].textContent;
-        if(text === equalText1 || text=== equalText2 || text=== equalText3){
+        if (labels.includes(text)) {
             document.getElementsByClassName("ytp-ad-skip-button")[0].click();
         } else {
             setTimeout(skipAd(), 1000);
@@ -37,11 +25,10 @@ function skipAd(){
     }
 }
 
-(function() {
+(function () {
     'use strict';
-    addNewStyle('.ytp-ad-overlay-slot {display:none !important;}');
-    setInterval(function (){
-        if(window.location.href.substring(0,30) == "https://www.youtube.com/watch?"){
+    setInterval(function () {
+        if (window.location.href.substring(0, 30) == "https://www.youtube.com/watch?") {
             skipAd();
         }
     }, 2500);
